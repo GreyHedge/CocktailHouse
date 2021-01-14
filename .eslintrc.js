4ec2351ca9c581b7d671f30e3c2aba2905a87f3d
GreyHedge <greyhedge15@gmail.com>
2021-01-15 00:03:08 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+};
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ]
+};
---
+++ App.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#f4f6f0"/>
+      <SafeAreaView style={styles.wrapper}>
+        <View style={styles.mainView}>
+          <Text style={styles.title}>CocktailHouse</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#f4f6f0',
+  },
+  mainView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#002d40',
+  },
+});
+
+export default App;
